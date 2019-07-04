bb0fb5b6e2c93916ec28e592e85034d0fa63e96a
Anton Barada <themrbarada@gmail.com>
2019-07-04 17:25:57 +0300

    finalize feature module isolation

--- projects/ngx-state-manager/src/lib/state-manager.module.ts
+++ projects/ngx-state-manager/src/lib/state-manager.module.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders, NgModule, Type } from '@angular/core';
 import { StateManagerEvents } from './events/events';
 import { ListenersSubscription } from './events/listeners-subscription';
 import { FeatureStateManager } from './feature-state-manager';
+import { StateManagerFeatureModule } from './state-manager-feature.module';
 import { StateManagerRootModule } from './state-manager-root.module';
 import { FEATURE_STATE_MANAGERS, ROOT_STATE_MANAGERS } from './tokens';
 
@@ -25,8 +26,9 @@ export class StateManagerModule {
 
   static forFeature(featureStateManagers: Type<any>[]): ModuleWithProviders {
     return {
-      ngModule: StateManagerRootModule,
+      ngModule: StateManagerFeatureModule,
       providers: [
+        StateManagerEvents,
         featureStateManagers,
         {
           provide: FEATURE_STATE_MANAGERS,
@@ -41,6 +43,6 @@ export class StateManagerModule {
 export function createManagerInstances(
   ...instances: FeatureStateManager<any>[]
 ) {
-  instances.forEach(i => i.onInit());
+  instances.forEach(i => typeof i.onInit === 'function' ? i.onInit() : void 0);
   return instances;
 }
